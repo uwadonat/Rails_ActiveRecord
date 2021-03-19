f27032ae655b0b3b5e1d5c9e4b1f18344faef770
uwadonat <uwadonat@gmail.com>
2021-03-19 08:05:19 -0400

    add column body on comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_105811) do
+ActiveRecord::Schema.define(version: 2021_03_19_115010) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_105811) do
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "body"
   end
 
   create_table "posts", force: :cascade do |t|
